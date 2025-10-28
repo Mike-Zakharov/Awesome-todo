a9eed20e435d1c1c60f61b6ac1899df8d628fd60
Mikhail Zakharov <mike.zakharov1990@gmail.com>
2025-10-28 12:35:02 +0300

    I created a HeaderTitle component that changes the title depending on the page opened.

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,10 +1,34 @@
+import { useEffect, useState } from "react";
 import "../styles/header.css";
 import { Icon } from "./icon.tsx";
+import { useLocation } from "react-router";
+import { HeaderTitle, type headerTitleProps } from "./header-title.tsx";
 
 export function Header() {
+  const location = useLocation();
+  const [titleType, setTitleType] = useState<headerTitleProps["title"]>(
+    location.pathname as headerTitleProps["title"]
+  );
+
+  const titleTypes = [
+    "/",
+    "/vital-task",
+    "/my-task",
+    "/task-categories",
+    "/account-info",
+  ] as const;
+  type TitleType = (typeof titleTypes)[number];
+
+  useEffect(() => {
+    if (titleTypes.includes(location.pathname as TitleType)) {
+      setTitleType(location.pathname as TitleType);
+    } else {
+      setTitleType("/");
+    }
+  }, [location.pathname]);
   return (
     <header className="header">
-      <div>Заголовок страницы</div>
+      <HeaderTitle title={titleType} />
       <div>
         <input type="search" placeholder="Search your task here..." />
         <button>Поиск</button>
